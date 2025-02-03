4530664a1981074ad8d022408dbbe80af38c17c1
Michael Corcias <michael.corcias@gelt.com>
2025-02-03 16:24:26 +0100

    avances entregable

---
+++ product.js
@@ -0,0 +1,48 @@
+
+
+let products = []
+let id = 0
+
+function resetProducts() {
+    products = []
+    id = 0
+}
+
+function addProduct(name, price) {
+    if (!name || !price) {
+        throw new Error("The name and the price are required")
+    }
+
+    if (products.some(product => product.name === name)) {
+        throw new Error("The product already exists")
+    }
+
+    id++
+    const newProduct = { id, name, price }
+    products.push(newProduct)
+    return newProduct
+}
+
+function removeProduct(id) {
+    const index = products.findIndex(product => product.id === id)
+
+    if(index === -1){
+        throw new Error("The id does not exists")
+    }
+
+    products.splice(index, 1)
+}
+
+function getProducts() {
+    return products
+}
+
+function getProductById(id) {
+    const product = products.find(product => product.id === id)
+    if (!product) {
+        throw new Error("Product not found");
+    }
+    return product
+}
+
+module.exports = { resetProducts, addProduct, getProducts, removeProduct, getProductById }
---
+++ product.test.js
@@ -0,0 +1,58 @@
+const { resetProducts, addProduct, removeProduct, getProducts, getProductById, pdateProduct } = require('./product')
+
+beforeEach(() => {
+    resetProducts()
+})
+
+describe("addProduct", () => {
+    it("should add a product", () => {
+        addProduct("Manzana", 2);
+        const products = getProducts();
+        expect(products.length).toBe(1);
+        expect(products[0]).toEqual({ id: 1, name: "Manzana", price: 2 });
+    })
+
+    it("should increment the id when adding a product", () => {
+        addProduct("Manzana", 2)
+        addProduct("Pera", 3)
+        const products = getProducts()
+        expect(products[0].id).toBe(1)
+        expect(products[1].id).toBe(2)
+    })
+
+    it("should throw an error if name or price is missing", () => {
+        expect(() => addProduct().toThrow("The name and the price are required"))
+        expect(() => addProduct("", 2).toThrow("The name and the price are required"))
+        expect(() => addProduct("Manzana",).toThrow("The name and the price are required"))
+    })
+
+    it("should throw an error if the product already exists", () => {
+        addProduct("Manzana", 2)
+        expect(() => addProduct("Manzana", 2).toThrow("The product already exists"))
+    })
+})
+
+
+describe("removeProduct", () => {
+    it("should remove a product by id", () => {
+        addProduct("Manzana", 2)
+        addProduct("Pera", 3)
+        removeProduct(1)
+        products = getProducts()
+        expect(products[0].name).toBe("Pera")
+    })
+})
+
+describe("getProductById", () => {
+    it("should return a product by id", () => {
+        const product = addProduct("Manzana", 2)
+        const id = product.id
+        const productById = getProductById(id)
+        expect(productById.name).toBe("Manzana")
+        expect(productById.price).toBe(2)
+    })
+
+    it("should throw an error if the product does not exist", () => {
+        expect(() => getProductById(999)).toThrow("Product not found")
+    })
+})
